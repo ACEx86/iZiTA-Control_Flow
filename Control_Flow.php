c07c5b420377acd598709119cea55e841775d0df
ACEx86 <42440432+ACEx86@users.noreply.github.com>
2025-12-12 03:43:05 +0200

    Refactor Get_Script_Depth function for clarity

--- Control_Flow.php
+++ Control_Flow.php
@@ -1225,16 +1225,14 @@ namespace iZiTA
          */
         Final Function Get_Script_Depth(): Int|Bool
         {
-            if(isset($this->Script_Depth) === True)
+            $Script_Depth = ($this->Script_Depth ?? False) ?: ($Script_Depth = 0);
+            if(is_int($Script_Depth) === True and $Script_Depth === $this->Script_Depth)
             {
-                $Script_Depth = 0;
-                $Script_Depth = ($this->Script_Depth ?? 0) ?: ($Script_Depth = 0);
-                if(is_int($Script_Depth) === True and $Script_Depth === $this->Script_Depth)
-                {
-                    return $Script_Depth;
-                }
+                return $Script_Depth;
+            }else
+            {
+                return False;
             }
-            return False;
         }
         //</editor-fold>
         //<editor-fold desc="Final Shared Functions">
